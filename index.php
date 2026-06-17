15464740046654a20351817b36107e64a4dee006
JustinasLa <95593194+JustinasLa@users.noreply.github.com>
2026-06-17 23:41:28 +0200

    favicon fix

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (!empty($mapQuery)) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>LetMeRent - Browse Listings</title>
-  <link rel="icon" type="image/svg+xml" href="favicon.svg">
+  <link rel="icon" type="image/svg+xml" href="favicon.svg?v=2">
   <link rel="stylesheet" href="styles.css">
   <link rel="stylesheet" href="index.css">
 </head>
